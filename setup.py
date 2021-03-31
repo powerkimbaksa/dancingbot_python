cc02c37b3f80d71db89d46e35dce82768c851924
applenongbu <hutk1726@naver.com>
2021-03-31 12:17:37 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
         author='applenongbu(TNB alba)',
         author_email= 'daefrica@kakao.com', 
         url='https://github.com/applenongbu/dancingbot_python', # github url
-        download_url        = 'https://github.com/applenongbu/dancingbot_python/archive/v0.1.tar.gz', # release 이름
+        download_url        = 'https://github.com/applenongbu/dancingbot_python/archive/master.zip', # release 이름
         install_requires    =  ["pyserial"], # 패키지 사용시 필요한 모듈
         packages            = find_packages(exclude = []),
         keywords            = ['dancingbot_python'], # 키워드
